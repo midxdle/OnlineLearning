70341d4e0358e9b059c090b23145baab1e549683
midxdle <sina.heydarian.sh@gmail.com>
2022-08-17 17:56:00 +0430

    database connection, middlewares, order of codes and some bugs has been fixed

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
 const express = require("express");
+const session = require('express-session');
+const expressValidator = require('express-validator');
+const cookieParser = require('cookie-parser');
+const passport = require("passport");
+const localStrategy = require('passport-local').Strategy;
+const createError = require('http-errors');
 const path = require('path');
-const flash = require("connect-flash");
-const db = require("./database/mongo");
+
+let db = require('./database/mongo');
+let homeRouter = require('./routes/home');
+let usersRouter = require('./routes/users');
 
 const app = module.exports = express();
-
-app.use(flash());
-app.use((req, res, next) => {
-  let messages = require("express-messages")(req, res);
-  res.locals.messages = (chunk, context, bodies, params) => {
-    return chunk.write(messages());
-  };
-  next();
-});
-
-let homeRouter = require('./routes/home');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//testing purpose
+app.use(express.static(path.join(__dirname, 'public')));
+//testing purpose
+
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+  errorFormatter : (param, msg, value) => {
+    let namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+app.use(require('connect-flash')());
+app.use( (req, res, next) => {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.get('*', (req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+})
+
 app.use('/', homeRouter);
+app.use('/users', usersRouter);
