a5177c4f319cd681af09374adf992891b6eefe42
midxdle <sina.heydarian.sh@gmail.com>
2022-09-04 08:50:19 +0430

    comments added and requiring db from mongo removed from index

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const expressValidator = require("express-validator");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
-const createError = require("http-errors");
 const path = require("path");
 
-let db = require("./database/mongo");
+// take routers
 let homeRouter = require("./routes/home");
 let usersRouter = require("./routes/users");
 let contentRouter = require("./routes/content");
+let profileRouter = require("./routes/profile");
+let adminRouter = require("./routes/admin");
+let teacherRouter = require("./routes/teacher");
 
+// use express
 const app = (module.exports = express());
 
+// set view directory path and view engine to render pages
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// parse URLs
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// set styles directory path
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
@@ -33,6 +39,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// errors handler
 app.use(
   expressValidator({
     errorFormatter: (param, msg, value) => {
@@ -52,17 +59,25 @@ app.use(
   })
 );
 
+// show errors
 app.use(require("connect-flash")());
 app.use((req, res, next) => {
   res.locals.messages = require("express-messages")(req, res);
   next();
 });
 
+// render app root for checking authentications
+//isAuthenticated method in routes
+//if user method in pug format
 app.get("*", (req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
 
+// define routes
 app.use("/", homeRouter);
 app.use("/users", usersRouter);
 app.use("/content", contentRouter);
+app.use("/profile", profileRouter);
+app.use("/admin", adminRouter);
+app.use("/teacher", teacherRouter);
