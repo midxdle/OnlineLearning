8891c81b46db455b05933589a15a1dd633ad4425
midxdle <sina.heydarian.sh@gmail.com>
2022-09-04 08:51:13 +0430

    buy model store students informations now

--- models/buy.js
+++ models/buy.js
@@ -1,13 +1,26 @@
 const mongoose = require("mongoose");
 
 let buySchema = mongoose.Schema({
-  courseID: {
+  contentID: {
     type: String,
     index: true,
   },
+  contentName: {
+    type: String,
+  },
   studentID: {
+    type: String,
+  },
+  stdUsername: {
+    type: String,
+  },
+  stdNumber: {
     type: String,
   },
 });
 
 let buy = (module.exports = mongoose.model("buy", buySchema));
+
+module.exports.createBuy = (newBuy, callback) => {
+  newBuy.save(callback);
+};
