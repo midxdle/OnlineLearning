81c4745f48d92c388e4c956e2e530b1336d76f79
midxdle <sina.heydarian.sh@gmail.com>
2022-09-04 08:55:10 +0430

    create new course function added

--- models/course.js
+++ models/course.js
@@ -17,3 +17,7 @@ let courseSchema = mongoose.Schema({
 });
 
 let course = (module.exports = mongoose.model("course", courseSchema));
+
+module.exports.createCourse = (newCourse, callback) => {
+  newCourse.save(callback);
+};
