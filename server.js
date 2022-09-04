5123b84a6677488b63870b6c0312c45cf342644a
midxdle <sina.heydarian.sh@gmail.com>
2022-09-04 08:59:29 +0430

    comments added

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require("./index");
 const http = require("http");
 
+// create server based on express
 const server = http.createServer(app);
+// server listening port
 server.listen(process.env.PORT || 8000);
 server.on("listening", () => {
   console.log("Server listening on http://localhost:%d", 8000);
