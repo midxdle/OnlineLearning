5c7787d8cb97c301c1a8db45266353ff0a4320a1
midxdle <sina.heydarian.sh@gmail.com>
2022-09-08 12:22:04 +0430

    contentID for routing to contents now will read from course model for better supporting in front

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const course = require("../models/course");
-const content = require("../models/content");
 
 // course page
 router.get("/", ensureAuthenticated, (req, res, next) => {
@@ -9,15 +8,9 @@ router.get("/", ensureAuthenticated, (req, res, next) => {
   course.find({}, (err, courses) => {
     if (err) throw err;
 
-    // find content to use content ID
-    content.find({}, (err, contents) => {
-      if (err) throw err;
-
-      res.render("home", {
-        title: "members",
-        courses: courses,
-        contents: contents,
-      });
+    res.render("home", {
+      title: "members",
+      courses: courses,
     });
   });
 });
